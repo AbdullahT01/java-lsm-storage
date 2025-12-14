44bc29dd255b264e7514b9fce0dab8af8b0338d9
Abdullah <abdullahtahad30@gmail.com>
2025-12-13 22:08:40 -0500

    feat: createSST and readSST functions
    - added the logic for these 2 functions, now we can flush everything
      inside .sst files instead of just clearing the tree.
    - called the functions inside the flush method inside the memtable class

--- src/com/lsm/MemTable.java
+++ src/com/lsm/MemTable.java
@@ -4,6 +4,7 @@ import java.util.TreeMap;
 public class MemTable {
     private final TreeMap<String, String> table;
     private int currentByteSize;
+    private static int segmentID = 0;
 
     private static final int MEMTABLE_BYTE_LIMIT = 4096;
 
@@ -31,8 +32,17 @@ public class MemTable {
     public void flush() {
         System.out.println("We hit the limit for this MemTable, will now flush!");
 
+        // creating the file using the createSSTable function
+        String fileName = "data-" + segmentID + ".sst";
+        SSTable sst = new SSTable();
+        sst.createSSTable(table, fileName);
+        sst.readSSTable(fileName);
+
         table.clear();
         currentByteSize = 0;
+        segmentID++;
+
         System.out.println("flush has been complete");
     }
+
 }
--- src/com/lsm/SSTable.java
+++ src/com/lsm/SSTable.java
@@ -1,2 +1,52 @@
+package com.lsm;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.TreeMap;
+
 public class SSTable {
+
+    public void createSSTable(TreeMap<String,String> data, String fileName){
+        try(
+                FileOutputStream fos = new FileOutputStream(fileName);
+                BufferedOutputStream bos = new BufferedOutputStream(fos);
+                DataOutputStream dos = new DataOutputStream(bos);
+        )
+        {
+                for(var entry : data.entrySet()){
+                    String key = entry.getKey();
+                    String value = entry.getValue();
+
+                    dos.writeUTF(key);
+                    dos.writeUTF(value);
+                }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void readSSTable(String fileName){
+        try(
+                FileInputStream fis = new FileInputStream(fileName);
+                BufferedInputStream bis = new BufferedInputStream(fis);
+                DataInputStream dis = new DataInputStream(bis);
+        )
+        {
+                while(dis.available() > 0){
+                    String key = dis.readUTF();
+                    String value = dis.readUTF();
+
+                    System.out.println("Key: " + key + " | Value: " + value);
+                }
+        }
+        catch (IOException e){
+            // here we will catch the end of line exception
+            throw new RuntimeException(e);
+        }
+    }
 }
